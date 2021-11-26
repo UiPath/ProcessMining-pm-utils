170fd888b7715a32264bc7d66fea5b4f7166d921
Celine Verhoef <celine.verhoef@uipath.com>
2021-11-26 14:42:03 +0100

    chore: changed snowflake date time formats to strings

--- macros/multiple_databases/to_time.sql
+++ macros/multiple_databases/to_time.sql
@@ -1,7 +1,7 @@
 {% macro to_time(attribute) %}
 
 {% if target.type == 'snowflake' %}
-    try_to_time({{ attribute }}, {{ var("time_format") }})
+    try_to_time({{ attribute }}, '{{ var("time_format") }}')
 {% elif target.type == 'sqlserver' %}
     try_convert(time, {{ attribute }}, {{ var("time_format") }})
 {% endif %}
