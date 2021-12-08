55d99a3b4dd8d238ae24bed9e838e24dab829e6c
Sjoerd van Heesbeen <sjoerd.van.heesbeen@uipath.com>
2021-12-08 16:34:10 +0100

    refactor: change DBT_SCHEMA to schema in copied macros

--- macros/generic_tests/test_edgecount.sql
+++ macros/generic_tests/test_edgecount.sql
@@ -5,7 +5,7 @@
 select
     abs(edgecount - casecount - eventcount) as diff_count
 from (select count(*) as edgecount from {{ model }}) as model_edges
-cross join (select count(*) as casecount from "{{ var('DBT_SCHEMA') }}"."{{ case_model }}")  as model_cases
-cross join (select count(*) as eventcount from "{{ var('DBT_SCHEMA') }}"."{{ event_model }}")  as model_events
+cross join (select count(*) as casecount from "{{ var('schema') }}"."{{ case_model }}")  as model_cases
+cross join (select count(*) as eventcount from "{{ var('schema') }}"."{{ event_model }}")  as model_events
 
 {% endmacro %}
--- macros/generic_tests/test_startplacecount_sinkplacecount.sql
+++ macros/generic_tests/test_startplacecount_sinkplacecount.sql
@@ -4,7 +4,7 @@
 
 select
     abs(startplacecount - casecount) as diff_startplacecount, abs(sinkplacecount - casecount) as diff_sinkplacecount
-from (select count(*) as casecount from "{{ var('DBT_SCHEMA') }}"."{{ compare_model }}") as model_cases
+from (select count(*) as casecount from "{{ var('schema') }}"."{{ compare_model }}") as model_cases
 cross join (select count(*) as startplacecount from {{ model }} where "From_activity" is null)  as model_startplace
 cross join (select count(*) as sinkplacecount from {{ model }} where "To_activity" is null)  as model_sinkplace
 
