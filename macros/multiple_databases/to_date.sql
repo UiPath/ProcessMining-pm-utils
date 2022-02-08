a410745902d7f337a15c3452018c5f310a6653d3
Maraise <maraise.veens@uipath.com>
2022-02-08 14:33:02 +0100

    refactor: update to_date and to_double
    for to_date and to_double first cast to varchar

--- macros/multiple_databases/to_date.sql
+++ macros/multiple_databases/to_date.sql
@@ -1,7 +1,7 @@
 {% macro to_date(attribute) %}
 
 {% if target.type == 'snowflake' %}
-    try_to_date({{ attribute }}, '{{ var("date_format") }}')
+    try_to_date(to_varchar({{ attribute }}), '{{ var("date_format") }}')
 {% elif target.type == 'sqlserver' %}
     try_convert(date, {{ attribute }}, {{ var("date_format") }})
 {% endif %}
--- macros/multiple_databases/to_double.sql
+++ macros/multiple_databases/to_double.sql
@@ -1,7 +1,7 @@
 {% macro to_double(attribute) %}
 
 {% if target.type == 'snowflake' %}
-    try_to_double({{ attribute }})
+    try_to_double(to_varchar({{ attribute }}))
 {% elif target.type == 'sqlserver' %}
     try_convert(float, {{ attribute }})
 {% endif %}
