859f5f1d9cdce9e68f05779a8a046c02ae6197f0
Dennis <dennis.sogeler@uipath.com>
2022-03-25 09:21:57 +0100

    refactor: reorder optional macro to reduce code duplication
    - moved typechecking if statement outside other if statement
    - changed comment to jinja style comment to prevent comment showing up in compiled version
    - removed newline in output

--- macros/generic/optional.sql
+++ macros/generic/optional.sql
@@ -2,49 +2,35 @@
 
 {%- set columns = adapter.get_columns_in_relation(source_table) -%}
 
--- Create list of column names.
+{# Create list of column names.#}
 {%- set column_names = [] -%}
 {%- for column in columns -%}
     {%- set column_names = column_names.append('"' + column.name + '"') -%}
 {%- endfor -%}
 
--- When the column is in the list, use the column, otherwise create the column with null values.
+{# When the column is in the list, use the column, otherwise create the column with null values.#}
 {% if optional_column in column_names -%}
-    {%- if data_type == 'boolean' -%}
-        {{ pm_utils.to_boolean(optional_column) }} as {{ optional_column }}
-    {%- elif data_type == 'date' -%}
-        {{ pm_utils.to_date(optional_column) }} as {{ optional_column }}
-    {%- elif data_type == 'double' -%}
-        {{ pm_utils.to_double(optional_column) }} as {{ optional_column }}
-    {%- elif data_type == 'integer' -%}
-        {{ pm_utils.to_integer(optional_column) }} as {{ optional_column }}
-    {%- elif data_type == 'time' -%}
-        {{ pm_utils.to_time(optional_column) }} as {{ optional_column }}
-    {%- elif data_type == 'datetime' -%}
-        {{ pm_utils.to_timestamp(optional_column) }} as {{ optional_column }}
-    {%- elif data_type == 'text' -%}
-        {{ pm_utils.to_varchar(optional_column) }} as {{ optional_column }}
-    {%- else -%}
-        {{ pm_utils.to_varchar(optional_column) }} as {{ optional_column }}
-    {%- endif -%}
+    {% set column_value = optional_column -%}
+{%- else -%}    
+    {% set column_value = 'null' -%}
+{%- endif -%}
+
+{%- if data_type == 'boolean' -%}
+    {{ pm_utils.to_boolean(column_value) }} as {{ optional_column }}
+{%- elif data_type == 'date' -%}
+    {{ pm_utils.to_date(column_value) }} as {{ optional_column }}
+{%- elif data_type == 'double' -%}
+    {{ pm_utils.to_double(column_value) }} as {{ optional_column }}
+{%- elif data_type == 'integer' -%}
+    {{ pm_utils.to_integer(column_value) }} as {{ optional_column }}
+{%- elif data_type == 'time' -%}
+    {{ pm_utils.to_time(column_value) }} as {{ optional_column }}
+{%- elif data_type == 'datetime' -%}
+    {{ pm_utils.to_timestamp(column_value) }} as {{ optional_column }}
+{%- elif data_type == 'text' -%}
+    {{ pm_utils.to_varchar(column_value) }} as {{ optional_column }}
 {%- else -%}
-    {%- if data_type == 'boolean' -%}
-        {{ pm_utils.to_boolean('null') }} as {{ optional_column }}
-    {%- elif data_type == 'date' -%}
-        {{ pm_utils.to_date('null') }} as {{ optional_column }}
-    {%- elif data_type == 'double' -%}
-        {{ pm_utils.to_double('null') }} as {{ optional_column }}
-    {%- elif data_type == 'integer' -%}
-        {{ pm_utils.to_integer('null') }} as {{ optional_column }}
-    {%- elif data_type == 'time' -%}
-        {{ pm_utils.to_time('null') }} as {{ optional_column }}
-    {%- elif data_type == 'datetime' -%}
-        {{ pm_utils.to_timestamp('null') }} as {{ optional_column }}
-    {%- elif data_type == 'text' -%}
-        {{ pm_utils.to_varchar('null') }} as {{ optional_column }}
-    {%- else -%}
-        {{ pm_utils.to_varchar('null') }} as {{ optional_column }}
-    {%- endif -%}
+    {{ pm_utils.to_varchar(column_value) }} as {{ optional_column }}
 {%- endif -%}
 
 {%- endmacro -%}
