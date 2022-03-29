6de337bd70679ae19f7cff06933377a79b89592a
Celine Verhoef <celine.verhoef@uipath.com>
2022-03-29 13:58:43 +0200

    feat: add ms to time format and put back variables per macro

--- macros/multiple_databases/to_time.sql
+++ macros/multiple_databases/to_time.sql
@@ -1,9 +1,9 @@
 {%- macro to_time(attribute) -%}
 
 {%- if target.type == 'snowflake' -%}
-    try_to_time(to_varchar({{ attribute }}), '{{ var("time_format", "hh24:mi:ss") }}')
+    try_to_time(to_varchar({{ attribute }}), '{{ var("time_format", "hh24:mi:ss.ff3") }}')
 {%- elif target.type == 'sqlserver' -%}
-    try_convert(time, {{ attribute }}, {{ var("time_format", 8) }})
+    try_convert(time, {{ attribute }}, {{ var("time_format", 14) }})
 {%- endif -%}
 
 {%- endmacro -%}
