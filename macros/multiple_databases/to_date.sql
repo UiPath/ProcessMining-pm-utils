c6d3e08012534f8f91b3c45259e1b639947e1f61
Dennis <dennis.sogeler@uipath.com>
2022-03-29 11:45:17 +0200

    feat: add default formats
    Added default formats so they do not need to be explicitly declared as variable in projects

--- macros/multiple_databases/to_date.sql
+++ macros/multiple_databases/to_date.sql
@@ -1,9 +1,9 @@
 {%- macro to_date(attribute) -%}
 
 {%- if target.type == 'snowflake' -%}
-    try_to_date(to_varchar({{ attribute }}), '{{ var("date_format") }}')
+    try_to_date(to_varchar({{ attribute }}), '{{ var("date_format", "YYYY-MM-DD") }}')
 {%- elif target.type == 'sqlserver' -%}
-    try_convert(date, {{ attribute }}, {{ var("date_format") }})
+    try_convert(date, {{ attribute }}, {{ var("date_format", 23) }})
 {%- endif -%}
 
 {%- endmacro -%}
--- macros/multiple_databases/to_time.sql
+++ macros/multiple_databases/to_time.sql
@@ -1,9 +1,9 @@
 {%- macro to_time(attribute) -%}
 
 {%- if target.type == 'snowflake' -%}
-    try_to_time(to_varchar({{ attribute }}), '{{ var("time_format") }}')
+    try_to_time(to_varchar({{ attribute }}), '{{ var("time_format", "hh24:mi:ss") }}')
 {%- elif target.type == 'sqlserver' -%}
-    try_convert(time, {{ attribute }}, {{ var("time_format") }})
+    try_convert(time, {{ attribute }}, {{ var("time_format", 8) }})
 {%- endif -%}
 
 {%- endmacro -%}
