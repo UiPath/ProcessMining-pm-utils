446f6cf475324ea3d63ad268d0d862281790f0bb
Dennis <dennis.sogeler@uipath.com>
2022-06-14 13:21:20 +0200

    fix: have test_type macros use the correct fieldname
    By default fieldnames are encapsulated in (extra) double quotes, these have to be removed
    fix: use lowercase in statements

--- macros/generic_tests/test_type_timestamp.sql
+++ macros/generic_tests/test_type_timestamp.sql
@@ -4,7 +4,7 @@ select *
 from Information_schema.Columns
 where Information_schema.Columns."TABLE_SCHEMA" = '{{ model.schema }}'
     and Information_schema.Columns."TABLE_NAME" = '{{ model.name }}'
-    and Information_schema.Columns."COLUMN_NAME" = '{{ column_name }}'
+    and Information_schema.Columns."COLUMN_NAME" = replace('{{ column_name }}', '"', '')
 {% if target.type == 'snowflake' %}
     and Information_schema.Columns."DATA_TYPE" <> 'TIMESTAMP_NTZ'
 {% elif target.type == 'sqlserver' %}
