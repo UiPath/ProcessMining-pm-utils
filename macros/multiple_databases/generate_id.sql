218155daabf0c773f784e9c4f233b03f7a49e19b
Celine Verhoef <celine.verhoef@uipath.com>
2022-07-14 11:06:07 +0200

    fix: make sequence unique to support parallel execution of models

--- macros/multiple_databases/generate_id.sql
+++ macros/multiple_databases/generate_id.sql
@@ -2,12 +2,12 @@
 
 {%- if target.type == 'snowflake' -%}
     use "{{ this.database }}"."{{ this.schema }}";
-    create or replace sequence seq;
+    create or replace sequence seq_{{ this.identifier }};
     create or replace table {{ this }} as (
         select
             {{ this }}.*,
             s.nextval as "{{ id_field }}"
-        from {{ this }}, table(getnextval(seq)) s
+        from {{ this }}, table(getnextval(seq_{{ this.identifier }})) s
     )
 {%- elif target.type == 'sqlserver' -%}
     alter table {{ this }}
