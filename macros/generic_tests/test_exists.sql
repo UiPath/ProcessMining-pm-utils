da0bd3bb172706bc3f90b8cb83fd9d76d24bd588
Celine Verhoef <celine.verhoef@uipath.com>
2022-07-20 14:09:06 +0200

    feat: updated test_exists to return a fail instead of error

--- macros/generic_tests/test_exists.sql
+++ macros/generic_tests/test_exists.sql
@@ -1,7 +1,12 @@
 {% macro test_exists(model, column_name) %}
 
-select {{ column_name }}
-from {{ model }}
-where 1 = 0
+{# Test fails if 0 records are returned. #}
+{{ config(fail_calc = 'case when count(*) = 0 then 1 else 0 end') }}
+
+select *
+from Information_schema.Columns
+where Information_schema.Columns."TABLE_SCHEMA" = '{{ model.schema }}'
+    and Information_schema.Columns."TABLE_NAME" = '{{ model.name }}'
+    and Information_schema.Columns."COLUMN_NAME" = replace('{{ column_name }}', '"', '')
 
 {% endmacro %}
