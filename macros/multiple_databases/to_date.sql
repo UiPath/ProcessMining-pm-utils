40ca408063a1948cb2628a7018d723ccb463e71e
Celine Verhoef <celine.verhoef@uipath.com>
2022-07-21 13:41:25 +0200

    feat: make to_date more robust to also handle datetime formats

--- macros/multiple_databases/to_date.sql
+++ macros/multiple_databases/to_date.sql
@@ -1,7 +1,12 @@
 {%- macro to_date(field) -%}
 
+{# Cast to date when the input is in a date or a datetime format. This is default behavior for SQL Server. #}
 {%- if target.type == 'snowflake' -%}
-    try_to_date(to_varchar({{ field }}), '{{ var("date_format", "YYYY-MM-DD") }}')
+    case
+        when try_to_date(to_varchar({{ field }}), '{{ var("date_format", "YYYY-MM-DD") }}') is null
+            then {{ pm_utils.date_from_timestamp(field) }}
+        else try_to_date(to_varchar({{ field }}), '{{ var("date_format", "YYYY-MM-DD") }}')
+    end
 {%- elif target.type == 'sqlserver' -%}
     try_convert(date, {{ field }}, {{ var("date_format", 23) }})
 {%- endif -%}
