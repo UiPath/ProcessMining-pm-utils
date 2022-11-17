76161d313413bdbabaaeb47d4b02386cb273cd5c
Celine Verhoef <celine.verhoef@uipath.com>
2022-11-17 14:17:49 +0100

    feat: improve comments on the run query part

--- macros/generic_tests/test_not_null.sql
+++ macros/generic_tests/test_not_null.sql
@@ -8,16 +8,17 @@ where Information_schema.Columns."TABLE_SCHEMA" = '{{ model.schema }}'
     and Information_schema.Columns."COLUMN_NAME" = replace('{{ column_name }}', '"', '')
 {% endset %}
 
+{# Execute the exists query. The record count is 1 if the field exists and 0 if it does not exist.#}
 {% set result_exists_query = run_query(exists_query) %}
 
-{# Execute the exists query. The result is 1 if the field exists and 0 if it does not exist. #}
+{# Get record count only when dbt is in execute mode to prevent compilation errors. #}
 {% if execute %}
 {% set record_count = result_exists_query.columns['record_count'].values()[0] %}
 {% else %}
-{% set record_count = -1 %}
+{% set record_count = 1 %}
 {% endif %}
 
-{# Only execute test when field exists. Otherwise execute a dummy test that always returns 0 records. #}
+{# Only execute test when field exists. Otherwise execute a dummy test that always succeeds. #}
 {% if record_count > 0 %}
 select {{ column_name }}
 from {{ model }}
