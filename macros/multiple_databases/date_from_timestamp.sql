d7017e5cbcfed538c2395cd108489589c76f0c26
Sven <sven.bego@uipath.com>
2022-11-24 11:26:33 +0100

    fix: use try_to_date() instead of to_date()
    Changed to_date() function to try_to_date() function in date_from_timestamp.sql

--- macros/multiple_databases/date_from_timestamp.sql
+++ macros/multiple_databases/date_from_timestamp.sql
@@ -1,7 +1,7 @@
 {%- macro date_from_timestamp(field) -%}
 
 {%- if target.type == 'snowflake' -%}
-    to_date({{ field }})
+    try_to_date({{ field }})
 {%- elif target.type == 'sqlserver' -%}
     try_convert(date, {{ field }})
 {%- endif -%}
