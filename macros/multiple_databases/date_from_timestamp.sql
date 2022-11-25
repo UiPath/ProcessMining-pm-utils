0f23273c5963bcedfcd4e4b331650425e4d76f81
Sven <sven.bego@uipath.com>
2022-11-25 11:01:34 +0100

    fix: revert try_to_date() to date()

--- macros/multiple_databases/date_from_timestamp.sql
+++ macros/multiple_databases/date_from_timestamp.sql
@@ -1,7 +1,7 @@
 {%- macro date_from_timestamp(field) -%}
 
 {%- if target.type == 'snowflake' -%}
-    try_to_date({{ field }})
+    to_date({{ field }})
 {%- elif target.type == 'sqlserver' -%}
     try_convert(date, {{ field }})
 {%- endif -%}
