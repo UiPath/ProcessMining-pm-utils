3c59667df58203bb461e93d88c46d22e9eb551a9
Celine Verhoef <celine.verhoef@uipath.com>
2023-01-10 15:21:11 +0100

    feat: log message when the test fails

--- macros/generic_tests/test_exists.sql
+++ macros/generic_tests/test_exists.sql
@@ -9,4 +9,25 @@ where "INFORMATION_SCHEMA"."COLUMNS"."TABLE_SCHEMA" = '{{ model.schema }}'
     and "INFORMATION_SCHEMA"."COLUMNS"."TABLE_NAME" = '{{ model.name }}'
     and "INFORMATION_SCHEMA"."COLUMNS"."COLUMN_NAME" = replace('{{ column_name }}', '"', '')
 
+{% set query %}
+    select count(*) as "test_record_count"
+    from "INFORMATION_SCHEMA"."COLUMNS"
+    where "INFORMATION_SCHEMA"."COLUMNS"."TABLE_SCHEMA" = '{{ model.schema }}'
+        and "INFORMATION_SCHEMA"."COLUMNS"."TABLE_NAME" = '{{ model.name }}'
+        and "INFORMATION_SCHEMA"."COLUMNS"."COLUMN_NAME" = replace('{{ column_name }}', '"', '')
+{% endset %}
+
+{% set result = run_query(query) %}
+
+{% if execute %}
+    {% set test_record_count = result.columns['test_record_count'].values()[0] %}
+{% else %}
+    {% set test_record_count = 1 %}
+{% endif %}
+
+{# User-friendly log message when the test fails. #}
+{% if test_record_count == 0 %}
+    {{ log("Message", True) }}
+{% endif %}
+
 {% endmacro %}
