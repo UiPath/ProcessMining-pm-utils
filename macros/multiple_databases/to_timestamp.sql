b92fa7b5c617e3c8ac7c1a534535dbf2cf442566
Silviu Stanimir <silviu.stanimir@uipath.com>
2023-02-22 09:37:30 +0100

    refactor: change datetime to datetime2 in SQL Server datetime format

--- macros/multiple_databases/to_timestamp.sql
+++ macros/multiple_databases/to_timestamp.sql
@@ -3,7 +3,7 @@
 {%- if target.type == 'snowflake' -%}
     try_to_timestamp(to_varchar({{ field }}), '{{ var("datetime_format", "YYYY-MM-DD hh24:mi:ss.ff3") }}')
 {%- elif target.type == 'sqlserver' -%}
-    try_convert(datetime, {{ field }}, {{ var("datetime_format", 21) }})
+    try_convert(datetime2, {{ field }}, {{ var("datetime_format", 21) }})
 {%- endif -%}
 
 {%- endmacro -%}
