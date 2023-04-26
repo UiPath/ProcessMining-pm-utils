1fa801d519585aa5645700e2b07d13645b77a371
Celine Verhoef <celine.verhoef@uipath.com>
2023-04-26 14:26:23 +0200

    feat: implement the log messages
    fixup: messages

--- macros/multiple_databases/to_double.sql
+++ macros/multiple_databases/to_double.sql
@@ -28,7 +28,9 @@
     {% endif %}
 
     {% if record_count > 0 %}
-        {{ log("Warning", True) }}
+        {% if var("log_result", False) == True %}
+            {{ log(tojson({'Key': 'ConvertDouble', 'Details': {'table_identifier': table.identifier, 'field': field, 'record_count': record_count}, 'Category': 'UserWarning', 'Message': 'Failed to convert \'' ~ table.identifier ~ '.' ~ field ~ '\' to a double for ' ~ record_count ~ ' records. Their values are set to NULL.'}), True) }}
+        {% endif %}
     {% endif %}
 {% endif %}
 
