466fa3adc35e2a18ec314e2dddf676b7a52d2983
Celine Verhoef <celine.verhoef@uipath.com>
2023-04-26 13:45:20 +0200

    feat: implement warnings on conversion when table arg is defined

--- macros/multiple_databases/to_boolean.sql
+++ macros/multiple_databases/to_boolean.sql
@@ -1,4 +1,4 @@
-{%- macro to_boolean(field) -%}
+{%- macro to_boolean(field, table) -%}
 
 {%- if target.type == 'snowflake' -%}
     {%- if field in ('true', 'false', '1', '0') -%}
@@ -13,5 +13,39 @@
         try_convert(bit, {{ field }})
     {%- endif -%}
 {%- endif -%}
+
+{# Warning if type casting will introduce null values for at least 1 record. #}
+{% if table is defined %}
+    {% set query %}
+    select
+        count(*) as "record_count"
+    from "{{ table.database }}"."{{ table.schema }}"."{{ table.identifier }}"
+    where {{ field }} is not null and
+        {% if target.type == 'snowflake' -%}
+            {%- if field in ('true', 'false', '1', '0') -%}
+                try_to_boolean('{{ field }}') is null
+            {%- else -%}
+                try_to_boolean(to_varchar({{ field }})) is null
+            {%- endif -%}
+        {% elif target.type == 'sqlserver' -%}
+            {%- if field in ('true', 'false', '1', '0') -%}
+                try_convert(bit, '{{ field }}') is null
+            {%- else -%}
+                try_convert(bit, {{ field }}) is null
+            {%- endif -%}
+        {%- endif -%}
+    {% endset %}
+
+    {% set result_query = run_query(query) %}
+    {% if execute %}
+        {% set record_count = result_query.columns['record_count'].values()[0] %}
+    {% else %}
+        {% set record_count = 0 %}
+    {% endif %}
+
+    {% if record_count > 0 %}
+        {{ log("Warning", True) }}
+    {% endif %}
+{% endif %}
 
 {%- endmacro -%}
--- macros/multiple_databases/to_integer.sql
+++ macros/multiple_databases/to_integer.sql
@@ -1,9 +1,35 @@
-{%- macro to_integer(field) -%}
+{%- macro to_integer(field, table) -%}
 
 {%- if target.type == 'snowflake' -%}
     try_to_number(to_varchar({{ field }}))
 {%- elif target.type == 'sqlserver' -%}
     try_convert(bigint, {{ field }})
 {%- endif -%}
+
+{# Warning if type casting will introduce null values for at least 1 record. #}
+{% if table is defined %}
+    {% set query %}
+    select
+        count(*) as "record_count"
+    from "{{ table.database }}"."{{ table.schema }}"."{{ table.identifier }}"
+    where {{ field }} is not null and
+        {% if target.type == 'snowflake' -%}
+            try_to_number(to_varchar({{ field }})) is null
+        {% elif target.type == 'sqlserver' -%}
+            try_convert(bigint, {{ field }}) is null
+        {%- endif -%}
+    {% endset %}
+
+    {% set result_query = run_query(query) %}
+    {% if execute %}
+        {% set record_count = result_query.columns['record_count'].values()[0] %}
+    {% else %}
+        {% set record_count = 0 %}
+    {% endif %}
+
+    {% if record_count > 0 %}
+        {{ log("Warning", True) }}
+    {% endif %}
+{% endif %}
 
 {%- endmacro -%}
