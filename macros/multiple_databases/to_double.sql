c1a072988586303df8fe3940c6974cf083ae3b9f
Maraise <maraise.veens@uipath.com>
2023-05-03 13:20:53 +0200

    fix: update double casting in sqlserver

--- macros/multiple_databases/to_double.sql
+++ macros/multiple_databases/to_double.sql
@@ -3,7 +3,12 @@
 {%- if target.type == 'snowflake' -%}
     try_to_double(to_varchar({{ field }}))
 {%- elif target.type == 'sqlserver' -%}
-    try_convert(float, {{ field }})
+    case
+        when len({{ field }}) > 0
+            then try_convert(float, {{ field }})
+        else
+            NULL
+    end
 {%- endif -%}
 
 {# Warning if type casting will introduce null values for at least 1 record. #}
