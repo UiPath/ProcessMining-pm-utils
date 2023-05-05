42d26c27b31b41bac434396ccbd3c47852044c13
Maraise <maraise.veens@uipath.com>
2023-05-05 15:40:15 +0200

    fix: update timestamp_from_date macro
    to cast empty strings/spaces to null

--- macros/multiple_databases/timestamp_from_date.sql
+++ macros/multiple_databases/timestamp_from_date.sql
@@ -1,18 +1,37 @@
 {%- macro timestamp_from_date(date_field) -%}
 
 {%- if target.type == 'snowflake' -%}
-    timestamp_from_parts({{ date_field }}, '0')
+    case
+        when len({{ date_field }}) > 0
+            then timestamp_from_parts({{ date_field }}, '0')
+        else
+            timestamp_from_parts(NULL, NULL)
+    end
 {%- elif target.type == 'sqlserver' -%}
-    datetime2fromparts(
-        datepart(year, {{ date_field }}),
-        datepart(month, {{ date_field }}),
-        datepart(day, {{ date_field }}),
-        0,
-        0,
-        0,
-        0,
-        3
-    )
+    case
+        when len({{ date_field }}) > 0
+            then datetime2fromparts(
+                    datepart(year, {{ date_field }}),
+                    datepart(month, {{ date_field }}),
+                    datepart(day, {{ date_field }}),
+                    0,
+                    0,
+                    0,
+                    0,
+                    3
+                )
+        else
+            datetime2fromparts(
+                NULL,
+                NULL,
+                NULL,
+                NULL,
+                NULL,
+                NULL,
+                NULL,
+                3
+            )
+    end
 {%- endif -%}
 
 {%- endmacro -%}
