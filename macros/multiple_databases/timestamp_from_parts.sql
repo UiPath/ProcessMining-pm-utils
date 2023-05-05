b917ac5d5837a746c620268a1862ee78d068f13e
Maraise <maraise.veens@uipath.com>
2023-05-05 15:40:08 +0200

    fix: update timestamp_from_parts macro
    to cast empty strings/spaces to null

--- macros/multiple_databases/timestamp_from_parts.sql
+++ macros/multiple_databases/timestamp_from_parts.sql
@@ -1,18 +1,37 @@
 {%- macro timestamp_from_parts(date_field, time_field) -%}
 
 {%- if target.type == 'snowflake' -%}
-    timestamp_from_parts({{ date_field }}, {{ time_field }})
+    case
+        when len({{ date_field }}) > 0 and len({{time_field}}) > 0
+            then timestamp_from_parts({{ date_field }}, {{ time_field }})
+        else
+            timestamp_from_parts(NULL, NULL)
+    end
 {%- elif target.type == 'sqlserver' -%}
-    datetime2fromparts(
-        datepart(year, {{ date_field }}),
-        datepart(month, {{ date_field }}),
-        datepart(day, {{ date_field }}),
-        datepart(hour, {{ time_field }}),
-        datepart(minute, {{ time_field }}),
-        datepart(second, {{ time_field }}),
-        datepart(millisecond, {{ time_field }}),
-        3
-    )
+    case
+        when len({{ date_field }}) > 0 and len({{time_field}}) > 0
+            then datetime2fromparts(
+                    datepart(year, {{ date_field }}),
+                    datepart(month, {{ date_field }}),
+                    datepart(day, {{ date_field }}),
+                    datepart(hour, {{ time_field }}),
+                    datepart(minute, {{ time_field }}),
+                    datepart(second, {{ time_field }}),
+                    datepart(millisecond, {{ time_field }}),
+                    3
+                )
+        else
+            datetime2fromparts(
+                NULL,
+                NULL,
+                NULL,
+                NULL,
+                NULL,
+                NULL,
+                NULL,
+                3
+            )
+    end
 {%- endif -%}
 
 {%- endmacro -%}
