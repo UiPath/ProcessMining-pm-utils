3bead18ea4ed177364a713a65a292f09239be7a5
William Lindsey <wiliam.lindsey@uipath.com>
2023-07-09 11:29:47 -0400

    fix: yes another try at string concat

--- macros/multiple_databases/as_varchar.sql
+++ macros/multiple_databases/as_varchar.sql
@@ -1,10 +1,7 @@
 {%- macro as_varchar(field) -%}
 
-  {%- macro dquote(s) -%}
-    "{{ s }}"
-  {%- endmacro -%}
 {%- if target.type == 'snowflake' -%}
-    to_varchar('{{ "\"" ~ field.split(".")|join("\"") ~ "\""}}')
+    to_varchar('{{ "\"" ~ field.split(".")|join("\".\"") ~ "\""}}')
 {%- elif target.type == 'sqlserver' -%}
     convert(nvarchar(2000), '{{ field }}')
 {%- else -%}
