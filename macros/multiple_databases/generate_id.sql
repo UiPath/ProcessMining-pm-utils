0a4da890282d86cb9c93f57ceafd5db0e2faaecd
William Lindsey <wiliam.lindsey@uipath.com>
2023-07-10 16:15:28 -0400

    fix: restore quotes in generate_id

--- macros/multiple_databases/generate_id.sql
+++ macros/multiple_databases/generate_id.sql
@@ -1,12 +1,12 @@
 {%- macro generate_id(id_field) -%}
 
 {%- if target.type == 'snowflake' -%}
-    use {{ this.database }}.{{ this.schema }};
+    use "{{ this.database }}"."{{ this.schema }}";
     create or replace sequence seq_{{ this.identifier }};
     create or replace table {{ this }} as (
         select
             {{ this }}.*,
-            s.nextval as {{ id_field }}
+            s.nextval as "{{ id_field }}"
         from {{ this }}, table(getnextval(seq_{{ this.identifier }})) s
     )
 {%- elif target.type == 'databricks' -%}
