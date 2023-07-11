facb256bada315e47442c8e51a9c804ce383b1ae
William Lindsey <wiliam.lindsey@uipath.com>
2023-07-11 19:12:05 -0400

    fix: enquoting datediff

--- macros/multiple_databases/datediff.sql
+++ macros/multiple_databases/datediff.sql
@@ -1,7 +1,7 @@
 {%- macro datediff(datepart, start_date_field, end_date_field) -%}
 
 {%- if target.type == 'snowflake' -%}
-    datediff({{ datepart }}, {{ pm_utils.enquote('start_date_field') }}, {{ pm_utils.enquote('end_date_field') }} }})
+    datediff({{ datepart }}, {{ pm_utils.enquote('start_date_field') }}, {{ pm_utils.enquote('end_date_field') }})
 {%- elif target.type == 'databricks' -%}
     cast({{end_date_field }} as DOUBLE)  - cast({{start_date_field }} as DOUBLE) * 1000
 {%- elif target.type == 'sqlserver' -%}
