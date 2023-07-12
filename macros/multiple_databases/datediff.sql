64bc6158958576b91ea4ad73b5e680d397aa1d05
William Lindsey <wiliam.lindsey@uipath.com>
2023-07-12 17:29:33 -0400

    fix: restore datadiff quoting

--- macros/multiple_databases/datediff.sql
+++ macros/multiple_databases/datediff.sql
@@ -1,7 +1,7 @@
 {%- macro datediff(datepart, start_date_field, end_date_field) -%}
 
 {%- if target.type == 'snowflake' -%}
-    datediff({{ datepart }}, {{ pm_utils.enquote(start_date_field) }}, {{ end_date_field.split(".")|join(".\"") ~ "\""}})
+    datediff({{ datepart }}, {{ start_date_field.split(".")|join(".\"") ~ "\"" }}, {{ end_date_field.split(".")|join(".\"") ~ "\""}})
 {%- elif target.type == 'databricks' -%}
     cast({{end_date_field }} as DOUBLE)  - cast({{start_date_field }} as DOUBLE) * 1000
 {%- elif target.type == 'sqlserver' -%}
