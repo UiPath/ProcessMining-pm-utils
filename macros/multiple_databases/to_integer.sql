d123327a11658010d8c630d2a4f243564107b15f
Celine Verhoef <celine.verhoef@uipath.com>
2023-08-07 13:12:04 +0200

    feat: databricks to int

--- macros/multiple_databases/to_integer.sql
+++ macros/multiple_databases/to_integer.sql
@@ -1,25 +1,35 @@
 {%- macro to_integer(field, relation) -%}
 
 {# Snowflake try_to function requires an expression of type varchar. #}
-{%- if target.type == 'snowflake' -%}
+{%- if target.type == 'databricks' -%}
+    try_cast({{ field }} as bigint)
+{%- elif target.type == 'snowflake' -%}
     try_to_number(to_varchar({{ field }}))
 {%- elif target.type == 'sqlserver' -%}
     case
         when len({{ field }}) > 0
             then try_convert(bigint, {{ field }})
         else
-             try_convert(bigint, null)
+            try_convert(bigint, null)
     end
 {%- endif -%}
 
 {# Warning if type casting will introduce null values for at least 1 record. #}
 {% if relation is defined %}
     {% set query %}
+    {% if target.type == 'databricks' -%}
+    select
+        count(*) as `record_count`
+    from `{{ relation.database }}`.`{{ relation.schema }}`.`{{ relation.identifier }}`
+    {%- elif target.type in ('sqlserver', 'snowflake') -%}
     select
         count(*) as "record_count"
     from "{{ relation.database }}"."{{ relation.schema }}"."{{ relation.identifier }}"
+    {% endif %}
     where {{ field }} is not null and
-        {% if target.type == 'snowflake' -%}
+        {% if target.type == 'databricks' -%}
+            try_cast({{ field }} as bigint) is null
+        {% elif target.type == 'snowflake' -%}
             try_to_number(to_varchar({{ field }})) is null
         {% elif target.type == 'sqlserver' -%}
             case
