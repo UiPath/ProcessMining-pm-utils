914f88602cf3b3872fa765fb1202b27064221034
Celine Verhoef <celine.verhoef@uipath.com>
2023-08-07 13:28:56 +0200

    feat: to_varchar databricks

--- macros/multiple_databases/to_varchar.sql
+++ macros/multiple_databases/to_varchar.sql
@@ -1,13 +1,15 @@
 {%- macro to_varchar(field) -%}
 
-{%- if target.type == 'snowflake' -%}
+{%- if target.type == 'databricks' -%}
+    cast({{ field }} as string)
+{%- elif target.type == 'snowflake' -%}
     to_varchar({{ field }})
 {%- elif target.type == 'sqlserver' -%}
     case
         when charindex(' ', {{ field }}) > 0
             then convert(nvarchar(2000), {{ field }})
         else
-            nullif(convert(nvarchar(2000), {{ field }}),'')
+            nullif(convert(nvarchar(2000), {{ field }}), '')
     end
 {%- endif -%}
 
