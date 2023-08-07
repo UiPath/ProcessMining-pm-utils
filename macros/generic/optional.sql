4cc84d324a3f66e803020e9477dfecd4c153535e
Celine Verhoef <celine.verhoef@uipath.com>
2023-08-07 15:39:16 +0200

    feat: update quotes in optional functions

--- macros/generic/optional.sql
+++ macros/generic/optional.sql
@@ -22,7 +22,11 @@
 {# Create list of column names.#}
 {%- set column_names = [] -%}
 {%- for column in columns -%}
-    {%- set column_names = column_names.append('"' + column.name + '"') -%}
+    {%- if target.type == 'databricks' -%}
+        {%- set column_names = column_names.append('`' + column.name + '`') -%}
+    {%- elif target.type in ('sqlserver', 'snowflake') -%}
+        {%- set column_names = column_names.append('"' + column.name + '"') -%}
+    {% endif %}
 {%- endfor -%}
 
 {# When the column is in the list, use the column, otherwise create the column with null values.#}
