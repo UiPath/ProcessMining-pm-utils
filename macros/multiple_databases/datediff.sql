b221b6cfe2aa51835534d8cb3d47627e5c8b2716
silviu stanimir <silviu.stanimir@uipath.com>
2023-09-29 17:16:50 +0200

    feat: add datediff option for Databricks

--- macros/multiple_databases/datediff.sql
+++ macros/multiple_databases/datediff.sql
@@ -9,6 +9,20 @@
     {%- else -%}
         datediff_big({{ datepart }}, {{ start_date_field }}, {{ end_date_field }})
     {%- endif -%}
+{%- elif target.type == 'databricks' -%}
+    {%- if datepart == 'millisecond' -%}
+        unix_millis(to_timestamp({{ end_date_field }})) - unix_millis(to_timestamp({{ start_date_field }}))
+    {%- elif datepart == 'second' -%}
+        unix_seconds(to_timestamp({{ end_date_field }})) - unix_seconds(to_timestamp({{ start_date_field }}))
+    {%- elif datepart == 'minute' -%}
+        bigint((unix_seconds(to_timestamp({{ end_date_field }})) - unix_seconds(to_timestamp({{ start_date_field }})))/60)
+    {%- elif datepart == 'hour' -%}
+        bigint((unix_seconds(to_timestamp({{ end_date_field }})) - unix_seconds(to_timestamp({{ start_date_field }})))/3600)
+    {%- elif datepart == 'day' -%}
+        bigint(datediff({{ end_date_field }}, {{ start_date_field }}))
+    {%- elif datepart == 'year' -%}
+        bigint(year({{ end_date_field }}) - year({{ start_date_field }}))
+    {%- endif -%}
 {%- endif -%}
 
 {%- endmacro -%}
