b123bcc3f549ecce5e9c2974d8619c4a86da009d
silviu stanimir <silviu.stanimir@uipath.com>
2023-10-03 09:26:05 +0200

    feat: implement equal row count test for Databricks

--- macros/generic_tests/test_equal_rowcount.sql
+++ macros/generic_tests/test_equal_rowcount.sql
@@ -2,9 +2,12 @@
 
 {{ config(fail_calc = 'coalesce(diff_count, 0)') }}
 
-select
-    abs(count_a - count_b) as diff_count
+select abs(count_a - count_b) as diff_count
 from (select count(*) as count_a from {{ model }}) as model_a
-cross join (select count(*) as count_b from "{{ model.schema }}"."{{ compare_model }}") as model_b
+{% if target.type == 'databricks' -%}
+    cross join (select count(*) as count_b from `{{ model.schema }}`.`{{ compare_model }}`) as model_b
+{% else -%}
+    cross join (select count(*) as count_b from "{{ model.schema }}"."{{ compare_model }}") as model_b
+{% endif -%}
 
 {% endmacro %}
