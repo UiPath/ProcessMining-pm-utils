6eb08cd3a8119d7a5015da01e914e3fa6cf96db8
Dennis-UiPath <dennis.sogeler@uipath.com>
2023-10-03 10:09:06 +0200

    fix: align datediff in weeks for SQL Server with Snowflake

--- macros/multiple_databases/datediff.sql
+++ macros/multiple_databases/datediff.sql
@@ -3,7 +3,12 @@
 {%- if target.type == 'snowflake' -%}
     datediff({{ datepart }}, {{ start_date_field }}, {{ end_date_field }})
 {%- elif target.type == 'sqlserver' -%}
-    datediff_big({{ datepart }}, {{ start_date_field }}, {{ end_date_field }})
+    {%- if datepart == 'week' -%}
+        {# To calculate week differences, weeks start by default on Sunday. Change to align with Snowflake (week starts on Monday) #}
+        datediff_big({{ datepart }}, dateadd(day, -1, {{ start_date_field }}), dateadd(day, -1, {{ end_date_field }}))
+    {%- else -%}
+        datediff_big({{ datepart }}, {{ start_date_field }}, {{ end_date_field }})
+    {%- endif -%}
 {%- endif -%}
 
 {%- endmacro -%}
