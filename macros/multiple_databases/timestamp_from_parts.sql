ad04186d4a7f4c804c63bca7a91e5efa890a2a8a
silviu stanimir <silviu.stanimir@uipath.com>
2023-10-09 15:35:22 +0200

    refactor: change time part input in timestamp_from_parts to string

--- macros/multiple_databases/timestamp_from_parts.sql
+++ macros/multiple_databases/timestamp_from_parts.sql
@@ -2,8 +2,8 @@
 
 {%- if target.type == 'snowflake' -%}
     case
-        when len({{ date_field }}) > 0 and len({{time_field}}) > 0
-            then timestamp_from_parts({{ date_field }}, {{ time_field }})
+        when len({{ date_field }}) > 0 and len({{ time_field }}) > 0
+            then timestamp_from_parts({{ date_field }}, try_to_time({{ time_field }}, '{{ var("time_format", "hh24:mi:ss.ff3") }}'))
         else
             timestamp_from_parts(null, null)
     end
@@ -14,10 +14,10 @@
                     datepart(year, {{ date_field }}),
                     datepart(month, {{ date_field }}),
                     datepart(day, {{ date_field }}),
-                    datepart(hour, {{ time_field }}),
-                    datepart(minute, {{ time_field }}),
-                    datepart(second, {{ time_field }}),
-                    datepart(millisecond, {{ time_field }}),
+                    datepart(hour, try_convert(time, {{ time_field }}, {{ var("time_format", 14) }})),
+                    datepart(minute, try_convert(time, {{ time_field }}, {{ var("time_format", 14) }})),
+                    datepart(second, try_convert(time, {{ time_field }}, {{ var("time_format", 14) }})),
+                    datepart(millisecond, try_convert(time, {{ time_field }}, {{ var("time_format", 14) }})),
                     3
                 )
         else
@@ -34,8 +34,8 @@
     end
 {%- elif target.type == 'databricks' -%}
     case
-        when length({{ date_field }}) > 0 and length({{time_field}}) > 0
-            then timestamp_millis(bigint(bigint(unix_date({{ date_field }})) * 86400000) + unix_millis({{ time_field }}))
+        when length({{ date_field }}) > 0 and length({{ time_field }}) > 0
+            then timestamp_millis(bigint(bigint(unix_date({{ date_field }})) * 86400000) + unix_millis(try_to_timestamp({{ time_field }}, '{{ var("time_format", "HH:mm:ss[.SSS]") }}')))
         else
             to_timestamp(null)
     end
