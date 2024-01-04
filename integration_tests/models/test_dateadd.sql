92e4c4fb0b7ff27e6a7a907493a504eec4be4ec0
Celine Verhoef <celine.verhoef@uipath.com>
2024-01-04 13:01:30 +0100

    test: extend integration tests with more than 25 days

--- integration_tests/models/test_dateadd.sql
+++ integration_tests/models/test_dateadd.sql
@@ -2,12 +2,12 @@ with Input_data as (
     select
         '2023-11-12 13:14:15.678' as `testdate`,
         null as `null_value`,
-        {{ pm_utils.to_integer('1') }} as `bigint_value`
-        
+        {{ pm_utils.to_integer('1') }} as `bigint_value`,
+        {{ pm_utils.to_integer('30') }} as `bigint_value_30`,
 )
 
 select
-    {# Add milliseconds, seconds, minutes, hours, days, months and years #}
+    {# Add 1 unit for every possible dateparts. #}
     {{ pm_utils.to_varchar(pm_utils.dateadd('millisecond', 1, '`testdate`')) }} as `add_milliseconds`,
     {{ pm_utils.to_varchar(pm_utils.dateadd('second', 1, '`testdate`')) }} as `add_seconds`,
     {{ pm_utils.to_varchar(pm_utils.dateadd('minute', 1, '`testdate`')) }} as `add_minutes`,
@@ -18,8 +18,13 @@ select
     {{ pm_utils.to_varchar(pm_utils.dateadd('quarter', 1, '`testdate`')) }} as `add_quarters`,
     {{ pm_utils.to_varchar(pm_utils.dateadd('year', 1, '`testdate`')) }} as `add_years`,
 
-    {# Use bigint as the value #}
-    {{ pm_utils.to_varchar(pm_utils.dateadd('year', '`bigint_value`', '`testdate`')) }} as `add_bigint`,    
+    {# Add 30 days to cover the scenario a bigint is required to store the amount of milliseconds. #}
+    {{ pm_utils.to_varchar(pm_utils.dateadd('day', 30, '`testdate`')) }} as `add_30_days_in_days`,
+    {{ pm_utils.to_varchar(pm_utils.dateadd('second', 2592000, '`testdate`')) }} as `add_30_days_in_seconds`,
+
+    {# Use a bigint datatype as the input for number of units to be added. #}
+    {{ pm_utils.to_varchar(pm_utils.dateadd('year', '`bigint_value`', '`testdate`')) }} as `add_bigint`,
+    {{ pm_utils.to_varchar(pm_utils.dateadd('day', '`bigint_value_30`', '`testdate`')) }} as `add_bigint_30_days`,
 
     {% if target.type == 'sqlserver' %}
         {# SQL Server uses datetime2 format, which has a precision of 7 digits #}
@@ -32,7 +37,10 @@ select
         '2023-12-12 13:14:15.6780000' as `add_months_expected`,
         '2024-02-12 13:14:15.6780000' as `add_quarters_expected`,
         '2024-11-12 13:14:15.6780000' as `add_years_expected`,
+        '2023-12-12 13:14:15.6780000' as `add_30_days_in_days_expected`,
+        '2023-12-12 13:14:15.6780000' as `add_30_days_in_seconds_expected`,
         '2024-11-12 13:14:15.6780000' as `add_bigint_expected`,
+        '2023-12-12 13:14:15.6780000' as `add_bigint_30_days_expected`,
     {% else %}
         '2023-11-12 13:14:15.679' as `add_milliseconds_expected`,
         '2023-11-12 13:14:16.678' as `add_seconds_expected`,
@@ -43,7 +51,10 @@ select
         '2023-12-12 13:14:15.678' as `add_months_expected`,
         '2024-02-12 13:14:15.678' as `add_quarters_expected`,
         '2024-11-12 13:14:15.678' as `add_years_expected`,
+        '2023-12-12 13:14:15.678' as `add_30_days_in_days_expected`,
+        '2023-12-12 13:14:15.678' as `add_30_days_in_seconds_expected`,
         '2024-11-12 13:14:15.678' as `add_bigint_expected`,
+        '2023-12-12 13:14:15.678' as `add_bigint_30_days_expected`,
     {% endif %}
     
     {# Use null as the date #}
