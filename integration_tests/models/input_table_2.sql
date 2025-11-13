f5fc3ee3188c11cd2eef7c994fd090913c69a5b4
Mozhgan <mozhgan.vazifehdoostirani@uipath.com>
2025-11-13 16:21:04 +0100

    feat: update dbt version

--- integration_tests/models/input_table_2.sql
+++ integration_tests/models/input_table_2.sql
@@ -1,7 +1,7 @@
 {# This creates a table that can be used in tests that require a relation as argument. #}
 select
-    5 as "Column_C",
-    3.5 as "Column_D",
+    {{ pm_utils.to_integer('5') }} as "Column_C",
+    {{ pm_utils.to_double('3.5') }} as "Column_D",
     null as "Column_E",
-    2 as "RN",
+    {{ pm_utils.to_integer('2') }} as "RN",
     'B' as "Column_A"
