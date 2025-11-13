69fdc9fcd93fa8b2e2daed1fae00a2e7ab423d39
Mozhgan <mozhgan.vazifehdoostirani@uipath.com>
2025-11-13 16:38:58 +0100

    feat: update test scenario

--- integration_tests/models/input_table_3.sql
+++ integration_tests/models/input_table_3.sql
@@ -3,5 +3,5 @@ select
     {{ pm_utils.to_integer('3') }} as "RN",
     {{ pm_utils.to_boolean('false') }} as "Column_B",
     {{ pm_utils.to_integer('7') }} as "Column_C",
-    {{ pm_utils.to_double('2.1') }} as "Column_E",
+    {{ pm_utils.to_boolean('false') }} as "Column_E",
     null as "Column_F"
